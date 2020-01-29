c92b6cd019cbc99c65a02f3ed9ad47cfd3091b89
rached1997 <hawejdene@gmail.com>
2020-01-29 14:31:00 +0100

    landing page igem

---
+++ src/app/app.routing.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    { path: 'home',             component: LandingComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
